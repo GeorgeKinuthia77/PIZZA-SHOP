7bcc8f6e272d126683a0e81b6ca65ccd10263433
GeorgeKinuthia77 <gnjoro077@gmail.com>
2019-08-08 10:20:13 +0300

    aded class registration and validations inmy forms.py

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -7,26 +7,26 @@ from wtforms import ValidationError
 
 class RegistrationForm(FlaskForm):
 
-    email = StringField('Rafiki, Enter your email address',validators=[Required(),Email()])
-    username = StringField( ' Rafiki, enter username',validators = [Required()])
+    email = StringField('Enter your email address',validators=[Required(),Email()])
+    username = StringField( 'enter username',validators = [Required()])
     password = PasswordField('Password',validators = [Required(),
     EqualTo('password_confirm',message = 'Passwords must match')])
     password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
     submit = SubmitField('Sign Up')
-    
+
     """ password and email validation """
-        
+
     def validate_email(self,data_field):
             if User.query.filter_by(email =data_field.data).first():
-                raise ValidationError('Oopsie rafiki, email is taken.')
+                raise ValidationError('Oops, email is taken.')
 
     def validate_username(self,data_field):
         if User.query.filter_by(username = data_field.data).first():
-            raise ValidationError('Oopsie rafiki, the username is taken')
+            raise ValidationError('Oops, the username is taken')
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Rafiki enter your email Address',validators=[Required(),Email()])
-    password = PasswordField('Rafiki enter your password',validators =[Required()])
+    email = StringField('enter your email Address',validators=[Required(),Email()])
+    password = PasswordField('enter your password',validators =[Required()])
     remember = BooleanField('Remember me')
     submit = SubmitField('Sign In')
